b2598b0c7bf037e9f3b58100d9f8366e92596b3b
NickGBR <gubarenko.nikita27@gmail.com>
2020-10-23 16:52:36 +0800

    User errors handling

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -1,24 +1,41 @@
-function getById(tableName){
+/**
+ * Данный метод получает данные из таблицы по id,
+ * если данный в поле ввода не переданы,
+ * метод возвращает все данные из таблицы.
+ * @param tableName
+ */
+function getById(tableName) {
     const request = new XMLHttpRequest();
+
     let id = document.getElementById('id_input').value;
-    request.open("GET", "/dbservice/"+ tableName + "/" + id, true);
+
+    request.open("GET", "/dbservice/" + tableName + "/" + id, true);
     request.setRequestHeader("Content-Type", "application/json");
 
     request.onreadystatechange = function () {
         if (request.readyState === XMLHttpRequest.DONE) {
-            const nameList = document.getElementById("name_list");
-            nameList.innerText = "";
-            if (request.status === 200) {
-                const data = JSON.parse(request.responseText);
-                console.log(data.hasOwnProperty(Object));
-                const text = JSON.stringify(data);
-                
-                let textNode = document.createTextNode(text);
-                let par = document.createElement('p');
-                par.appendChild(textNode);
-                nameList.appendChild(par);
 
+            if (request.status === 200) {
+                if (request.responseText.length === 0) {
+                    alert("Эллимент с " + id + " не найден!")
+                } else {
+                    const nameList = document.getElementById("name_list");
+                    nameList.innerText = "";
+                    if (request.status === 200) {
+                        const data = JSON.parse(request.responseText);
+                        console.log(data.hasOwnProperty(Object));
+                        const text = JSON.stringify(data);
+
+                        let textNode = document.createTextNode(text);
+                        let par = document.createElement('p');
+                        par.appendChild(textNode);
+                        nameList.appendChild(par);
+
+                    }
+                }
             }
+        } else if (request.status === 400) {
+            alert("Введите число!")
         }
     }
 
